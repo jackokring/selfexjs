a3757112d3a999054c6a759b908bccc908605fd2
Simon Jackson (Netbook) <jackokring@gmail.com>
2015-07-08 23:00:33 +0100

    Added document write 4 html

--- pack.js
+++ pack.js
@@ -38,7 +38,8 @@ function compress(uncompressed) {
 decomp = 'var W,H,A,K=\'function decompress() {\n "use strict";\n // Build the .\n var i,\n  = [],\n w,\n ,\n k,\n  = "",\n  = 256;\n for (i = 0; i < 256; i += 1) {\n [i] = String.fromCharCode(i);\n }\n \n w = .(0);\n  = w;\n for (i = 1; i < .length; i += 1) {\n k = .(i);\n if ([k]) {\n  = [k];\n }  {\n if (k === ) {\n  = w + w.(0);\n }  {\n  null;\n }\n }\n \n  += ;\n \n // Add w+[0] to the .\n [++] = w + .(0);\n \n w = ;\n }\n  ;\n };\',W=\'\'.split(\'\'),H=\'dictionary|compressed|entry|result|charAt|dictSize|return|else\'.split(\'|\');for(A in H)K=K.split(W[A]).join(H[A]);eval(K);'
 
 module.exports = {
-	pack: function(input) {
+	pack: function(input, html = false) {
+		if(html) return '<script>'+decomp+'var J=\''+compress(input.toString())+'\';document.write(decompress(J));</script>';
 		return decomp+'var J=\''+compress(input.toString())+'\';eval(decompress(J));';
 	}
 };
