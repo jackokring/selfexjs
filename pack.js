99735359536103f0c64783d500f89faab114ca21
Simon Jackson (Netbook) <jackokring@gmail.com>
2015-07-11 01:46:04 +0100

    Fixed syntax, fix logicals later

--- pack.js
+++ pack.js
@@ -2,45 +2,51 @@ var ug = require('uglify-js');
 var fs = require('fs');
 var async = require('async');
 
+function no(arg) {
+	return typeof arg === 'undefined';
+}
+
 //supply file names and functions for generating string with one object argument
 //A really ANNOYING way of Thread.yield(); --> Origination of Callback Hell
-function cache(file, args = null) { 
-	var fn = function(a) { return ""; };//do nothing
+function cache(file, args) { 
+	if(no(args)) args = null;
+	var fn = function() { return ""; };//do nothing
 	if(!(file instanceof Array)) file = [file];//each file
 	return async.reduce(file, "",
 		function(memo, item, callback) {
-			if(file instanceof Function) {
-				fn = file;
+			if(item instanceof Function) {
+				fn = item;
 				callback(memo);
 				return;
 			}
-			fs.readFile(item, { encoding: utf8 },
+			fs.readFile(item, { encoding: 'utf8' },
 				function(err, data) {
 					if(!err) {
 						callback(null, memo + data);
 						return;
 					}
 					var result = fn(item, args);
-					fs.writeFile(item, result, { encoding: utf8 },
+					fs.writeFile(item, result, { encoding: 'utf8' },
 						function(err) {
 							if(err) console.log(err);
 	    					}
 					);
 					callback(memo + result);
 				}
-			});
+			);
 		},
 		function(err, result){
 			if(err) console.log(err + " <" + result + ">");//unlikely
 		}
 	);
-};
+}
 
 function flush(file) {
 	fs.unlinkSync(file);
-};
+}
 
-function compress(uncompressed, splice = null) {
+function compress(uncompressed, splice) {
+	if(no(splice)) splice = null;
         // Build the dictionary.
         var i,
             dictionary = {},
@@ -61,9 +67,9 @@ function compress(uncompressed, splice = null) {
 
 		//Also dictionary persistance for relative coding of JSON
 		//data sourcing (maybe not worth the server side loading).
-	};
+	}
 
-	uncompressed = morph(new Buffer(uncompressed));//utf8
+	uncompressed = morph(new Buffer(uncompressed, 'utf8'));//utf8
  
         for (i = 0; i < uncompressed.length; i += 1) {
             c = uncompressed[i];
@@ -88,7 +94,8 @@ function compress(uncompressed, splice = null) {
         return result;
 };
 
-function decompress(compressed, json = null) {
+function decompress(compressed, json) {
+	if(no(json)) json = null
         // Build the dictionary.
         var i,
             dictionary = [],
@@ -102,7 +109,7 @@ function decompress(compressed, json = null) {
         }
         function merge(bin) {
 		return bin;//later
-	};
+	}
  
         w = String.fromCharCode(compressed[0]);
         result = w;
@@ -127,15 +134,17 @@ function decompress(compressed, json = null) {
         }
 	result = merge(new Buffer(result, 'binary')).toString('utf8');
         return result;
-};'
+}
 
 var decomp = cache([	function() {
 				//in main node directory
-				ug.minify(decompress.toSting(), {fromString: true});
+				ug.minify(decompress.toString(), {fromString: true});
 			},
 			".decomp.js"]);
 
-function pack(input, html = true, head = true) {
+function pack(input, html, head) {
+	if(no(html)) html = true;
+	if(no(head)) head = true;
 	if(html) return '<script>'+((head)?decomp:'')+';var J=\''+compress(input.toString())+'\';document.write(decompress(J));</script>';
 	return ((head)?decomp:'')+';var J=\''+compress(input.toString())+'\';eval(decompress(J));';
 }
@@ -143,19 +152,25 @@ function pack(input, html = true, head = true) {
 //This function packs down editable scripts in the "editable" directory to compressed
 //files in the server root. The "editable" directory is in the server root.
 //Quite likely a less compiler option soon.
-function packCache(files, html = true, args = null, prefix = "editable/") {
+function packCache(files, html, args, prefix) {
+	if(no(html)) html = true;
+	if(no(args)) args = true;
+	if(no(prefix)) prefix = "editable/";
 	files.unshift(function(item, args) {
 		return pack(fs.readFile(prefix + item, { encoding: utf8 }), html, false);	
 	});
 	return ((html)?'<script>':'') + decomp + ';' + ((html)?'</script>':'') + cache(files, args); 
-};
+}
 
-function cachePage(fileTot, files, html = true, args = null, prefix = "editable/") {
+function cachePage(fileTot, files, html, args, prefix) {
+	if(no(html)) html = true;
+	if(no(args)) args = true;
+	if(no(prefix)) prefix = "editable/";
 	return cache([	function() {
 				return packCache(files, html, args, prefix);
 			},
-			fileTot]
-);
+			fileTot]);
+}
 
 module.exports = {
 	pack: pack,
@@ -166,4 +181,4 @@ module.exports = {
 	//also sets template element names to find <name>...</name> with JSON nesting
 	generate: null,//a combination of file cache pack and setting source
 	live: null//a utility auto update JS+JSON maker, makes some innerHTML in server push ajax with single client eval
-};
+}
